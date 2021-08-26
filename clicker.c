d995dc501a45cf44d4561b1cefd3d04a2e545ffe
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-26 14:17:33 +0530

    Update clicker.c

--- clicker.c
+++ clicker.c
@@ -1,4 +1,7 @@
 int number1, number2, sum;
-printf("Enter two number: ");
+printf ("Enter two number: ");
+scanf ("&d &d", number1, number2 );
+sum = number1 + number2
+printf ("ansar %d", sum);
 
 
