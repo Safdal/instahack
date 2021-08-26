dd1e1d1bfa0d6267c5bd3fb994cb602168ec765b
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-26 22:07:32 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,2 +1,11 @@
+#include <stdio.h>
 
-printf ("enter a three number")
+int main() {
+             float num1, num2, num3, sum ;
+             printf ("enter a three number");
+             scanf ("%f, %f, %f", &num1, &num2, &num3);
+             sum = num1 + num2 + num3 /3;
+             printf ("your average number : %f"sum);
+             
+           } 
+
