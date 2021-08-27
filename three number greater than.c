a3af681abc81c1f7609ed48236aac2d278a0bb6c
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-27 10:13:07 +0530

    Update three number greater than.c

--- three number greater than.c
+++ three number greater than.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+int main() {
+             int num1, num2, num3;
+             printf ("ennder three number");
+             scanf ("%d %d %d", &num1, &num2, &num3);
+
+
+             if (num1 > num2  &&  num1 > num3)
+             printf ("greater than : %d",num1);
+
+
+             if (num2 > num1  &&  num2 > num3)
+             printf ("greater than : %d",num2);
+
+             if (num3 > num1 &&  num3 > num2)
+             printf ("greater than : %d",num3);
+           }
