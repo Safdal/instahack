4a75fb9dc9300c4e2ca1d8dfc9a7915330d0ff12
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-27 08:59:29 +0530

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main() {
+             int num;
+             printf ("enter a number");
+             scanf ("%d", num);
+             if (num < 0 ) {
+             printf ("you have ender positive number")
+             }else(num > 0){
+             printf ("you have ander negative number")
+             }
+             
+           }
