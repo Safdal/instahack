c5e723396d13c581bebce7bca1dec178c39bfaf1
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-27 09:30:54 +0530

    Create tesst3.c

---
+++ tesst3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+             int num1, num2;
+             printf ("enter two number");
+             scanf ("%d %d", &num1, & num2);
+             if (num1 > num2){
+             printf ("greater than : %d", num1);
+             }else{
+             printf ("less than : %d", num2)
+             }
+           }
