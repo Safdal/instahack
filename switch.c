ace9ae4aa943372a0505d5c807f9318ebab5f0e1
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-27 19:42:33 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+           int choice;
+           printf ("1 for porotta \n2 for mandhi \n3 for fried chicken \n4 race");
+           scanf ("%d", choice);
+           switch (choice){
+
+
+           case 1:
+           printf ("you have ender porotta");
+           break;
+
+           case 2:
+           printf ("you have ender mandhi");
+           break;
+
+           case 3:
+           printf ("you have ender fried chicken");
+           break;
+
+           case 4:
+           printf ("you have ender race")
+           brrak;
+
+           default:
+           printf ("you are a fool")
+
+           }
+          }
