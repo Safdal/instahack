e0b5faa3228b10c157e04337c0942bdd03c48c6c
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-27 23:18:13 +0530

    Create loop2.c

---
+++ loop2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+           int number,i,sum = 0;
+           printf ("ender a number");
+           scanf ("%d", &number);
+
+
+           for (i = 1; i < = number; i++) {
+           sum = sum + 1;
+           }
+
+           printf ("%d :", sum)
+          }
