7f12996fe952d700c35d631a1746cd4d4e061a75
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-28 15:30:57 +0530

    Create scanf loop.c

---
+++ scanf loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+           int i[5];
+           int num;
+           printf ("enter five number")
+           for (i = 0; i < 5; i++){
+           scanf ("%d", &num[i])
+           }
+           for (i = 0 ; i < 5; i++){
+           printf ("you have ender %d \n", num[i])
+           }
+          }
