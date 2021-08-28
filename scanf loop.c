42abdffc71a066230be7423f4b37d9242e1c33bd
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-28 15:39:40 +0530

    Update scanf loop.c

--- scanf loop.c
+++ scanf loop.c
@@ -3,12 +3,12 @@
 int main(){
            int i[100];
            int num, limit;
-           printf ("enter array limit")
-           printf ("enter number")
+           printf ("enter array limit");
+           printf ("enter number");
            for (i = 0; i < limit; i++){
-           scanf ("%d", &num[i])
+           scanf ("%d", &num[i]);
            }
            for (i = 0 ; i < limit; i++){
-           printf ("you have ender %d \n", num[i])
+           printf ("you have ender %d \n", num[i]);
            }
           }
