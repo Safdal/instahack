cc3503a9bf63f4d26e8f3fdb6204dc8713406aff
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-28 12:17:27 +0530

    Create loop prime not prime.c

---
+++ loop prime not prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+            int num, i, flag = 0; 
+            printf ("enter a number");
+            scanf ("%d",&num);
+
+
+            for (i = 2, i < n/2, i++){
+            if ( n % i == 0) {
+            flag = 1
+            break;
+             }
+            }
+            
+           if (flag == 0){
+           printf ("prime number");
+
+
+            }elsr{
+            printf ("not prime number");
+            }
+           }
