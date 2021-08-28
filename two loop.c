e96c3520969c7469567852ef511d7b574acd76e1
Safdal <65144086+Safdal@users.noreply.github.com>
2021-08-28 12:42:49 +0530

    Create two loop.c

---
+++ two loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+            int num, j, i;
+            printf ("enter a number");
+            scanf ("%d",&num);
+            for (i = 1; i < num; i++){
+
+            for (j = 0; j < i; j++){
+            printf ("* ");
+             }
+            
+            printf ("\n")
+
+            }
+           }
